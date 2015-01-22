9148ad97716393a030092d3f9dfd7bc48652d6ed
syukou <m.taguchi@leihauoli.com>
2015-01-22 16:00:31 +0900

    update files -> grant base sass ADD

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,31 @@
 module.exports = function(grunt) {
 	var pkg = grunt.file.readJSON('package.json');
-	//グラントタスクの設定
 	grunt.initConfig({
-		//sassの設定
 		sass: {
-			ddist: {
-				files: {
-					'css/base.css': 'sass/base.scss',
-					'css/common.css': 'sass/common.scss',
-					'css/module.css': 'sass/module.scss',
-					'css/page_blog.css': 'sass/page_blog.scss',
-					'css/page_post.css': 'sass/page_post.scss'
-				}
+			compile: {
+				expand: true,
+				flatten: true,
+				src: ['sass/*.scss'],
+				dest: 'css',
+				ext: '.css'
 			}
 		},
-		//watchの設定
 		watch: {
-			dev: {
-				files: [
-					'sass/base.scss',
-					'sass/common.scss',
-					'sass/module.scss',
-					'sass/page_blog.scss',
-					'sass/page_post.scss'
-				],
+			sass: {
+				files: ['sass/*.scss'],
 				tasks: ['sass']
+			},
+			options: {
+				livereload: true
 			}
 		}
 	});
 
-
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	var taskName;
+	for(taskName in pkg.devDependencies) {
+		if(taskName.substring(0, 6) == 'grunt-') {
+			grunt.loadNpmTasks(taskName);
+		}
+	}
+	grunt.registerTask('default', ['watch']);
 };
